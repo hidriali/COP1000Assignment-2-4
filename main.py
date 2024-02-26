ff89c93f4b2505d8959d9bb567faef432763d5a5
hidriali <159208504+hidriali@users.noreply.github.com>
2024-02-26 22:34:20 +0000

    AD

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 # input statements
-salary = float(input())
-numDependents = float(input())
+salary = float(1250.0)
+numDependents =2
 
 # calculate taxes here
 
+#find the the value of a virable named stateTax.
+stateTax=(salary*6.5/100)
+
+#find the value of a vairable named federalTax.
+fedralTax=(salary*28.0/100)
+
+#find the value of a vairable named depentetDeductation.
+dependentDeducation=(salary*2.5/100)*numDependents
+
+#find the value of a vairable named totalWithholding.
+totalWithholding=stateTax + fedralTax + dependentDeducation
+
+#find the a value of a vairable named takeHomePay.
+takeHomePay= salary-totalWithholding
+
 # output statements
+#display each named vairable and it's value in seprate lines.
+print("State Tax: $", str(stateTax))
+print("Fedral Tax: $", str(fedralTax))
+print("Dependents: $", str(dependentDeducation))
 print("Salary: $" + str(salary))
 print("Take Home Pay: $" + str(takeHomePay))
